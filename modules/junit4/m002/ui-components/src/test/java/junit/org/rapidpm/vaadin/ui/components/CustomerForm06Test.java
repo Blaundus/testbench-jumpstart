7165528af88bf8e3e91b09dfdda360576338d2e8
Sven Ruppert <sven.ruppert@gmail.com>
2017-12-14 11:06:52 +0100

    junit4 - m002

---
+++ modules/junit4/m002/ui-components/src/test/java/junit/org/rapidpm/vaadin/ui/components/CustomerForm06Test.java
@@ -0,0 +1,19 @@
+package junit.org.rapidpm.vaadin.ui.components;
+
+import com.vaadin.ui.themes.ValoTheme;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class CustomerForm06Test extends CustomerFormPageObject {
+
+  @Test
+  public void test001() {
+    getDriver().get(url);
+    Assert.assertTrue(saveButton()
+                          .getClassNames()
+                          .contains("v-button-" + ValoTheme.BUTTON_PRIMARY));
+  }
+}
